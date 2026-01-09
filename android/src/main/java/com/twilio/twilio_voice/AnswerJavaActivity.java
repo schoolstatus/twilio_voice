0ce7b649d7bc586155e1b65fed050c28b115c5fd
Colby Peyton <colby.peyton@gmail.com>
2026-01-09 10:02:28 -0600

    add 16kb page size support for android build

--- android/src/main/java/com/twilio/twilio_voice/AnswerJavaActivity.java
+++ android/src/main/java/com/twilio/twilio_voice/AnswerJavaActivity.java
@@ -114,7 +114,7 @@ public class AnswerJavaActivity extends AppCompatActivity {
                     checkPermissionsAndAccept();
                     break;
                 case Constants.ACTION_END_CALL:
-                    Log.d(TAG, "ending call" + activeCall != null ? "True" : "False");
+                    Log.d(TAG, "ending call " + (activeCall != null ? "True" : "False"));
                     if (activeCall == null) {
                         Log.d(TAG, "No active call to end. Returning");
                         finishAndRemoveTask();
